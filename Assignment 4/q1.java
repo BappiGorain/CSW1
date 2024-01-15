e8c950416dd681cfcb95f8522a1317ec763684fa
BappiGorain <146017226+BappiGorain@users.noreply.github.com>
2024-01-15 21:38:44 +0530

    code

---
+++ Assignment 4/q1.java
@@ -0,0 +1,9 @@
+public class q1
+{
+    public static void main(String args[])
+    {
+        int i = 10;
+        Integer a = Integer.valueOf(i);
+        System.out.println("Wrapper class of int: " + a);
+    }
+}
